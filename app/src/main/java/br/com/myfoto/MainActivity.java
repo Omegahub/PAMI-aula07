df20e6ae12a3f2be5845e8043d30c0d24c271b7c
Omegahub <Omegahub2019@gmail.com>
2019-09-16 21:43:43 -0300

    aula 07-myphoto

---
+++ app/src/main/java/br/com/myfoto/MainActivity.java
@@ -0,0 +1,48 @@
+package br.com.myfoto;
+
+import android.content.Intent;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
+import java.net.URI;
+
+public class MainActivity extends AppCompatActivity {
+
+    Uri uri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onCapturar(View v){
+        File diretorio = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        String nomeImagem  = diretorio.getPath() + "/" + System.currentTimeMillis() + ".jpg";
+        uri = Uri.fromFile(new File(nomeImagem));
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+        startActivityForResult(intent,52);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == 52 && resultCode == RESULT_OK) {
+            Toast.makeText(this, "Imagem carturada!!", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+            intent.setData(uri);
+            this.sendBroadcast(intent);
+        }
+        else{
+            Toast.makeText(this,"Imagem não capturada",Toast.LENGTH_LONG).show();
+        }
+    }
+}
